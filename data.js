df88acaa071dd4cdea0c375b03650d26095376f5
MRutkowski00 <141546768+MRutkowski00@users.noreply.github.com>
2023-08-09 20:42:30 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,427 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-przedpokj",
+      "name": "Przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09950387016839635,
+          "pitch": 0.046341593942795356,
+          "rotation": 0,
+          "target": "1-salon"
+        },
+        {
+          "yaw": 0.2946500618925665,
+          "pitch": 0.04327919033037375,
+          "rotation": 0,
+          "target": "3-gara"
+        },
+        {
+          "yaw": -0.37895741484948786,
+          "pitch": 0.03872427188766281,
+          "rotation": 0,
+          "target": "2-azienka-parter"
+        },
+        {
+          "yaw": -0.20293779974706005,
+          "pitch": 0.0396874391044264,
+          "rotation": 0,
+          "target": "4-hol-pietro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.060163262893105696,
+          "pitch": 0.04971696486404298,
+          "title": "Schowek",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.103379516592325,
+          "pitch": 0.07765105958325513,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        },
+        {
+          "yaw": 2.789808620261365,
+          "pitch": 0.06765185667999063,
+          "rotation": 0,
+          "target": "3-gara"
+        },
+        {
+          "yaw": -3.0089077183219146,
+          "pitch": 0.08266336285673681,
+          "rotation": 0,
+          "target": "2-azienka-parter"
+        },
+        {
+          "yaw": -2.8626515779059822,
+          "pitch": 0.08352681661836847,
+          "rotation": 0,
+          "target": "4-hol-pietro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6558110551084493,
+          "pitch": 0.08630086925497515,
+          "title": "Schowek",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-azienka-parter",
+      "name": "Łazienka parter",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8640592111233296,
+          "pitch": 0.023637879098423298,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-gara",
+      "name": "Garaż",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.045896269411351,
+          "pitch": -0.0360577015488488,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-hol-pietro",
+      "name": "Hol piętro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03771663098256717,
+          "pitch": -0.015539648651142102,
+          "rotation": 0,
+          "target": "6-sypialnia"
+        },
+        {
+          "yaw": 0.6310886988188962,
+          "pitch": -0.021786336216230495,
+          "rotation": 0,
+          "target": "7-pokj-1"
+        },
+        {
+          "yaw": -1.4492874888837655,
+          "pitch": 0.48875412157440756,
+          "rotation": 0,
+          "target": "0-przedpokj"
+        },
+        {
+          "yaw": -2.783987612437244,
+          "pitch": -0.01859079805834085,
+          "rotation": 0,
+          "target": "5-azienka-pitro"
+        },
+        {
+          "yaw": 2.724843602509888,
+          "pitch": 0.000790722685858114,
+          "rotation": 0,
+          "target": "8-pokj-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-azienka-pitro",
+      "name": "Łazienka piętro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.247724848992842,
+          "pitch": -0.05989413022778045,
+          "rotation": 0,
+          "target": "4-hol-pietro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-sypialnia",
+      "name": "Sypialnia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.861785018323598,
+          "pitch": -0.005603708272468921,
+          "rotation": 0,
+          "target": "4-hol-pietro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pokj-1",
+      "name": "Pokój 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.534691739763458,
+          "pitch": -0.07528265226591024,
+          "rotation": 0,
+          "target": "4-hol-pietro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pokj-2",
+      "name": "Pokój 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.566780262871312,
+          "pitch": -0.028994130868341017,
+          "rotation": 0,
+          "target": "4-hol-pietro"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "spacer-360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
